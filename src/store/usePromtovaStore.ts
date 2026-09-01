f9599001d930eb306eee7b740806fd84e769cb07
Dzemitry <dzemitryagent@gmail.com>
2026-09-01 23:42:57 +0300

    refactor: add compatibility store facade

---
+++ src/store/usePromtovaStore.ts
@@ -0,0 +1,6 @@
+export { usePromtovaStore } from './promptStore';
+export type { PromptStoreState as PromtovaState } from './promptStore';
+export { useThemeStore, applyTheme, presetThemeIds } from './themeStore';
+export { useUIStore } from './uiStore';
+export type { CustomTheme } from '../shared/types';
+export type { Toast } from './uiStore';
